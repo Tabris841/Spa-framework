d7a4519792bab09d85a4b9534ffe09a4388d2df7
Tabris841 <shadowtabris84@gmail.com>
2016-01-17 20:56:54 +0200

    finish dashboard

--- Framework/app/appConfig.js
+++ Framework/app/appConfig.js
@@ -1,4 +1,6 @@
-﻿angular.module('app').config(function ($provide) {
+﻿"use strict";
+
+angular.module('app').config(function ($provide) {
     $provide.decorator("$exceptionHandler", ["$delegate", function ($delegate) {
         return function (exception, cause) {
             $delegate(exception, cause);
