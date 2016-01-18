e7a0bc633d838ea19cca537c8b779a9702f9b74d
Tabris841 <shadowtabris84@gmail.com>
2016-01-18 09:00:57 +0200

    error with $modal

--- Framework/app/directives/wwaDashboardDirective.js
+++ Framework/app/directives/wwaDashboardDirective.js
@@ -14,19 +14,60 @@ angular.module('app').directive('wwaDashboard', [function () {
                 margins: [20, 20],
                 outerMargin: false,
                 pushing: true,
-                floating: true,
+                floating: false,
                 swapping: false
             };
 
-            scope.widgets = [
+            scope.widgetDefinitions = [
                 {
-                    title: 'First',
-                    sizeX: 3,
-                    sizeY: 3,
-                    row: 0,
-                    col: 0,
-                    template: '<wwa-temperature></wwa-temperature>'
+                    title: 'Temperature',
+                    settings: {
+                        sizeX: 3,
+                        sizeY: 3,
+                        minSizeX: 2,
+                        minSizeY: 2,
+                        template: '<wwa-temperature></wwa-temperature>',
+                        widgetSettings: {
+                            id: 1000,
+                            templateUrl: 'app/dialogs/wwaSelectLocationTemplate.html',
+                            controller: 'wwaSelectLocationController'
+                        }
+                    }
+                },
+                {
+                    title: 'Inventory',
+                    settings: {
+                        sizeX: 5,
+                        sizeY: 3,
+                        minSizeX: 2,
+                        minSizeY: 2,
+                        template: '<wwa-inventory></wwa-inventory>',
+                        widgetSettings: {
+                            id: 1002,
+                            templateUrl: 'app/dialogs/wwaSelectLocationTemplate.html',
+                            controller: 'wwaSelectLocationController'
+                        }
+                    }
+                },
+                {
+                    title: 'Employee',
+                    settings: {
+                        sizeX: 5,
+                        sizeY: 3,
+                        minSizeX: 2,
+                        minSizeY: 2,
+                        template: '<wwa-employee></wwa-employee>',
+                        widgetSettings: {
+                            id: 5000,
+                            templateUrl: 'app/dialogs/wwaSelectEmployeeTemplate.html',
+                            controller: 'wwaSelectEmployeeController'
+                        }
+                    }
                 }
+            ];
+
+            scope.widgets = [
+                
             ];
         }
     }
